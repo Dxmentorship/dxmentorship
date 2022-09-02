9e97741b8ec66c3d810ca4e0099ab6fbf0b2411e
kenny-io <ekeneeze3@gmail.com>
2022-09-02 19:41:56 +0400

    SHMG

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
           name="description"
           content="An online program design to charge you into devrel"
         />
-        {/* <meta property="og:url" content="https://www.devex.training" />
+        <meta property="og:url" content="https://www.devex.training" />
         <meta property="og:type" content="website" />
         <meta
           property="og:title"
@@ -31,12 +31,12 @@ export default function Home() {
         <meta
           name="twitter:image"
           content="https://res.cloudinary.com/kennyy/image/upload/v1662128709/mentor_ltu3vc.jpg"
-        /> */}
+        />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@kenny_io" />
         <meta
           name="twitter:title"
-          content="Online DevRel Mentorship Program designed to charge you ..."
+          content="DevRel Mentorship Program designed to charge you ..."
         />
         <meta
           name="twitter:description"
