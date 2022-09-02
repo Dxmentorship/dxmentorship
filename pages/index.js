2aa0e20974cdd385c07d3f9902854c9e81c09fe1
kenny-io <ekeneeze3@gmail.com>
2022-09-02 19:36:03 +0400

    add Twitter Card social preview

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
           name="description"
           content="An online program design to charge you into devrel"
         />
-        <meta property="og:url" content="https://www.devex.training" />
+        {/* <meta property="og:url" content="https://www.devex.training" />
         <meta property="og:type" content="website" />
         <meta
           property="og:title"
@@ -28,6 +28,21 @@ export default function Home() {
           content="Get equipped with knowledge and information that will propel you into a fulfilling Developer Advocacy career."
         />
         <meta property="og:image" content="/images/mentor.jpg" />
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/kennyy/image/upload/v1662128709/mentor_ltu3vc.jpg"
+        /> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@kenny_io" />
+        <meta
+          name="twitter:title"
+          content="Online DevRel Mentorship Program designed to charge you ..."
+        />
+        <meta
+          name="twitter:description"
+          content="Get equipped with knowledge and information that will propel you into a fulfilling Developer Advocacy career."
+        />
+        <meta name="twitter:image" content={OGImage} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
