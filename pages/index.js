321e69c6bbee6d5c1fb8a0abb5c75aaa6c2ef725
kenny-io <ekeneeze3@gmail.com>
2022-09-02 19:16:58 +0400

    Update preview vars

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import DXTProcess from "../components/OurProcess";
 import Testimonials from "../components/Testimonials";
 import Newsletter from "../components/Newsletter";
 export default function Home() {
+  const OGImage =
+    "https://res.cloudinary.com/kennyy/image/upload/v1662128709/mentor_ltu3vc.jpg";
   return (
     <div className={styles.container}>
       <Head>
@@ -14,7 +16,7 @@ export default function Home() {
           name="description"
           content="An online program design to charge you into devrel"
         />
-        <meta property="og:url" content="https://www.devex.training/" />
+        <meta property="og:url" content="https://www.devex.training" />
         <meta property="og:type" content="website" />
         <meta
           property="og:title"
@@ -23,14 +25,9 @@ export default function Home() {
         <meta name="twitter:card" content="summary" />
         <meta
           property="og:description"
-          content="Get equipped with knowledge and information that will propel you into a fulfilling Developer Advocacy career. We'll be working with industry experts and real world scenarios to prepare you for the job you've always wanted!"
+          content="Get equipped with knowledge and information that will propel you into a fulfilling Developer Advocacy career."
         />
-        <meta
-          property="og:image"
-          content={
-            "https://res.cloudinary.com/kennyy/image/upload/v1662128709/mentor_ltu3vc.jpg"
-          }
-        />
+        <meta property="og:image" content={OGImage} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
