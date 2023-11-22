9759265e3728e100e8c4850809aed1c57a259739
Joshua Omobola <omobolathejoshua@gmail.com>
2023-11-22 04:21:00 +0100

    fix build error

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,7 +1,7 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
-export async function get() {
+export async function GET() {
   const posts = await getCollection('posts');
   return rss({
     title: 'Astro Learner | Blog',
