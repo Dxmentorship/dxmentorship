aac557249ccb85282ba4a5dbb5321dce1dc05bfa
Joshua <66284362+kohasummons@users.noreply.github.com>
2025-03-12 20:33:57 +0100

    Discard changes to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import netlify from '@astrojs/netlify/functions';
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,16 +10,16 @@ export default defineConfig({
   markdown: {
     drafts: true,
     shikiConfig: {
-      theme: 'css-variables',
-    },
+      theme: 'css-variables'
+    }
   },
   shikiConfig: {
     wrap: true,
     skipInline: false,
-    drafts: true,
+    drafts: true
   },
   site: 'https://dxmentorship.com',
   integrations: [tailwind(), sitemap(), mdx()],
-  adapter: netlify(),
+  adapter: netlify()
 });
 export const prerender = false;
